094d87411171d80af6b13c6da2a49554b5f4060d
sheezamannazeer <sheezaman7@gmail.com>
2025-08-02 00:36:38 +0530

    Switch back to deployed backend for local development

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,10 +7,7 @@ const connectDB=async()=>{
             throw new Error("MONGO_URI is required");
         }
         
-        await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected successfully");
     }
     catch(error){
